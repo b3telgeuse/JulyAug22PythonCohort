3ba7101ac74d9da9752a2d4161f1dfa434573021
Adam Ulrich <ulrichac00@gmail.com>
2022-07-27 16:40:58 -0400

    Add files via upload

---
+++ assignments/week2/dojoPets/dojo_pets.py
@@ -0,0 +1,38 @@
+class Ninja:
+    def __init__(self, first_name, last_name, pet, treats, pet_food):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.pet = pet
+        self.treats = treats
+        self.pet_food = pet_food
+
+    def walk()
+
+    def feed()
+
+    def bathe()
+
+
+
+class Pet:
+    def __init__(self, name, type, tricks, 100, 50):
+        self.name = name
+        self.type = type
+        self.tricks = tricks
+        self.heatlh = 100
+        self.energy = 50
+
+    def sleep()
+
+    def eat()
+
+    def play()
+
+    def noise ()
+
+
+
+Adam = Ninja("Adam", "Ulrich", "Cat", "sooo many treats", "super yummy food")
+Morris = Pet("Morris", "Orange Tabby", "no tricks, he lazy cat", 90, 10)
+
+Ninja(Adam).walk().feed().bathe()
